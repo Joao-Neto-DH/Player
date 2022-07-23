fad87ed41ac248cf7b1bdb79c64ec471617264d4
Joao-Neto-DH <joaolima882018@gmail.com>
2022-07-23 11:26:55 +0100

    Animando barra de controles

--- assets/js/animation.js
+++ assets/js/animation.js
@@ -1,6 +1,9 @@
 const cover = document.querySelector('.cover');
 const playlist = document.querySelector('.playlist');
 const btn_list = document.querySelector('[data-list]');
+const controller = document.querySelector('.controller');
+const controllerHideClassName = 'controller-closed';
+let timer = setTimeout(hideController,5*1000);
 
 cover.addEventListener('click', openPlaylist);
 btn_list.addEventListener('click', openPlaylist);
@@ -8,7 +11,18 @@ window.addEventListener('load', () => {
     cover.style.display = playlist.classList.contains('playlist-closed') ? 'none' : '';
 });
 
+window.addEventListener('mousemove', (event) => {
+    // console.log(timer);
+    controller.classList.remove(controllerHideClassName);
+    clearTimeout(timer);
+    timer = setTimeout(hideController,5*1000);
+});
+
 function openPlaylist(event) {
     playlist.classList.toggle('playlist-closed');
     cover.style.display = playlist.classList.contains('playlist-closed') ? 'none' : '';
+}
+
+function hideController() {
+    controller.classList.add(controllerHideClassName);
 }
